48f97bded0d318043a9b7a7fcfc384ecb4e21001
Manisha Manimaran <manisha.x.manimaran@gsk.com>
2019-08-07 19:31:49 +0530

    commit

--- src/app/active-draft-protocol/active-draft-protocol.component.ts
+++ src/app/active-draft-protocol/active-draft-protocol.component.ts
@@ -11,6 +11,9 @@ import { uploadStatus } from './../upload-protocol/upload-protocol.component';
 export class ActiveDraftProtocolComponent implements OnInit {
   showprotocol : boolean;
   showpop = true;
+  therapauticArea ;
+  phase;
+  studyType;
   
 
   constructor(private router: Router){}
@@ -22,6 +25,16 @@ export class ActiveDraftProtocolComponent implements OnInit {
   {
     this.showprotocol = true;
     this.showpop = false;
+    this.therapauticArea = '';
+    this.studyType = '';
+    this.phase = '';
+  }
+
+  cancel()
+  {
+    this.therapauticArea = '';
+    this.studyType = '';
+    this.phase = '';
   }
 
   ngOnInit() {
--- src/app/view-protocol/view-protocol.component.ts
+++ src/app/view-protocol/view-protocol.component.ts
@@ -7,6 +7,14 @@ import { Router } from '@angular/router';
 })
 export class ViewProtocolComponent implements OnInit {
 
+  selectedAll: any;;
+  measures= [
+    { selected: false,title:"The RIGHT study: Risk stratification with Image Guidance of HMG coa reductase inhibitor Therapy", id: "ZEU-DGU-242CE", area :"Cardiovascular"},
+    { selected: false,title:"Assessment of prospective CYP2C19 genotype guided Dosing of Anti-Platelet Therapy in Percutaneous Coronary Intervention (ADAPT)", id: "KSZ-3T3KU8Z", area :"Cardiovascular"},
+    { selected: false,title:"A Phase 1, Single Ascending Dose, Randomized, Double-Blind, Placebo-Controlled Study to Evaluate the Safety, Tolerability, Pharmacokinetics, and Pharmacodynamics of Xisomab 3G3 in Healthy Adult Subjects", id: "3G3-15-01", area :"Cardiovascular"},
+    { selected: false,title:"Endovascular exclusion of  Abdominal Aortic Aneursyms in high risk patients", id: "KZHG-YIEGND1214", area :"Cardiovascular"},
+ 
+];
   
   constructor(private router: Router) { }
   ngOnInit() {
@@ -14,5 +22,16 @@ export class ViewProtocolComponent implements OnInit {
   generateRisk()
   {
     this.router.navigate(["About"]);
+  }
+
+  selectAll() {
+    for (var i = 0; i < this.measures.length; i++) {
+      this.measures[i].selected = this.selectedAll;
+    }
+  }
+  checkIfAllSelected() {
+    this.selectedAll = this.measures.every(function(item:any) {
+        return item.selected == true;
+      })
   }
 }
